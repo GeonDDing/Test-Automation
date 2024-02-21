ab2eb3344a5a43ab0a95d7ea49f802887ac35a4a
GeonDDing <paran1011@naver.com>
2024-02-21 21:08:13 +0900

    Fixed packages contents

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ from setuptools import setup, find_packages
 setup(
     name='example',
     version='0.1.0',
-    packages=find_packages(include=['Common', 'Common.*', 'API', 'API.*', 'Selenium', 'Selenium.*'])
+    packages=find_packages(include=['Common', 'Common.*', 'API', 'API.*', 'WebTesting', 'WebTesting.*'])
 )
 
