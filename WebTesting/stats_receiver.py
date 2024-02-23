6d0176f5afba7598e04417269cf8ba4e6a5aae54
GeonDDing <paran1011@naver.com>
2024-02-23 17:51:54 +0900

    stats sender, receiver 예외 처리 코드 수정

--- WebTesting/stats_receiver.py
+++ WebTesting/stats_receiver.py
@@ -4,82 +4,58 @@ import time
 import multiprocessing
 from stats_sender import StatsSender
 
+
 class StatsReceiver():
     def stats_receiver(self, queue):
-        is_backupsource_flag = False
-        is_replacesource_flag = False
         is_evergreen_flag = True
-        prev_source_layer = None
-        prev_source_stat = None
-        while not queue.get() == 'quit':
+        prev_source_layer = prev_source_stat = None
+        while True:
             try:
                 get_stats = queue.get()
-                chidx = get_stats[0]+1
-                if int(chidx) > 10:
-                    chidx = f"0{chidx}"
-                else:
-                    chidx = f"00{chidx}"
-                source_layer = get_stats[1]
-                source_stat = get_stats[2]
-                
-                if source_layer == '0':
-                    if prev_source_layer == '1':
-                        print(f"#{chidx} Restored to the Primary source")
-                    elif prev_source_layer == '2':
-                        print(f"#{chidx} Source changed (source:#0)")
-                    if source_stat == '-1' and is_evergreen_flag:
-                        print(f"#{chidx} Evergreen occurred.")
-                        is_evergreen_flag = False
-                    elif not source_stat == '-1':
-                        if not prev_source_stat == source_stat:
-                            if not is_evergreen_flag:
-                                print(f"#{chidx} Evergreen recovered")
-                                is_evergreen_flag = True
-                        print(f'#{chidx} Primary Source : {get_stats[3]}')
-                elif source_layer == '1':
-                    if prev_source_layer == '0' or prev_source_layer == '2':
-                        print(f"#{chidx} User replaced source, Input Changed")
-                    if source_stat == '-1' and is_evergreen_flag:
-                        print(f"#{chidx} Evergreen occurred")
-                        is_evergreen_flag = False
-                    elif not source_stat == '-1':
-                        if not prev_source_stat == source_stat:
-                            if not is_evergreen_flag:
-                                print(f"#{chidx} Evergreen recovered")
-                                is_evergreen_flag = True
-                        print(f'#{chidx} Replaced Source : {get_stats[3]}')
-                elif source_layer == '2':
-                    if prev_source_layer == '0':
-                        print(f"#{chidx} Source changed (source:#1)")
-                    elif prev_source_layer == '1':
-                        print(f"#{chidx} Restored to the Backup source")
-                    if source_stat == '-1' and is_evergreen_flag:
-                        print(f"#{chidx} Evergreen occurred.")
-                        is_evergreen_flag = False
-                    elif not source_stat == '-1':
-                        if not prev_source_stat == source_stat:
-                            if not is_evergreen_flag:
-                                print(f"#{chidx} Evergreen recovered")
-                                is_evergreen_flag = True
-                        print(f'#{chidx} Backup Source : {get_stats[3]}')
+                if get_stats == 'quit':
+                    break
+                chidx = f"0{get_stats[0]+1:02}"
+                source_layer, source_stat = get_stats[1:3]
 
-                prev_source_layer = source_layer
-                prev_source_stat = source_stat
+                if prev_source_layer is not None and source_layer != prev_source_layer:
+                    if prev_source_layer == '0':
+                        print(f"#{chidx} User replaced source, Input Changed" if source_layer ==
+                              '1' else f"#{chidx} Source changed (source:#1)")
+                    elif prev_source_layer == '1':
+                        print(
+                            f"#{chidx} Restored to the {'Primary' if source_layer == '0' else 'Backup'} source")
+                    elif prev_source_layer == '2':
+                        print(f"#{chidx} User replaced source, Input Changed" if source_layer ==
+                              '1' else f"#{chidx} Source changed (source:#0)")
+                if source_stat == '-1' and is_evergreen_flag:
+                    print(f"#{chidx} Evergreen occurred.")
+                    is_evergreen_flag = False
+                elif source_stat != '-1':
+                    if not is_evergreen_flag and prev_source_stat != source_stat:
+                        print(f"#{chidx} Evergreen recovered.")
+                        is_evergreen_flag = True
+                    print(
+                        f'#{chidx} {"Primary" if source_layer == "0" else "Backup" if source_layer == "2" else "Replaced"} Source : {get_stats[3]}')
+
+                prev_source_layer, prev_source_stat = source_layer, source_stat
                 time.sleep(2)
 
             except Exception as e:
                 print("An error occurred:", e)
+
 
 if __name__ == '__main__':
     stats_queue = multiprocessing.Queue()
     test = StatsReceiver()
     test2 = StatsSender()
 
-    sender_process = multiprocessing.Process(target=test2.stats_sender, args=(stats_queue,))
-    receiver_process = multiprocessing.Process(target=test.stats_receiver, args=(stats_queue,))
-    
+    sender_process = multiprocessing.Process(
+        target=test2.stats_sender, args=(stats_queue,))
+    receiver_process = multiprocessing.Process(
+        target=test.stats_receiver, args=(stats_queue,))
+
     sender_process.start()
     receiver_process.start()
-    
-    sender_process.join() 
+
+    sender_process.join()
     receiver_process.join()
--- WebTesting/stats_sender.py
+++ WebTesting/stats_sender.py
@@ -12,47 +12,62 @@ class StatsSender():
                 os.path.realpath(__file__))  # 현재 경로만 추출
             config_path = os.path.join(
                 script_directory, 'config.ini')  # 현재 경로에서 config.ini 찾음
-            config = configparser.ConfigParser()  # config.ini 파싱
-            config.read(config_path)  # config.ini 파싱 후 일기
+            config = configparser.ConfigParser()
+            config.read(config_path)
             self.url = config.get('Webpage', 'url')
 
         except Exception as e:
             print("An error occurred:", e)
-        
+
     def stats_sender(self, queue):
         output_info = None
-        while True:
-                # 지금은 channel index 를 prameter로 입력하지만 추후 실행 중인 채널을 자동으로 찾을 수 있도록 변경
+        max_retries = 3
+        while max_retries > 0:
             try:
                 chidx = 0
                 stat_response = requests.get(
                     f"{self.url}:900{chidx}/stats")
-
             except requests.exceptions.ConnectionError as e:
-                print('A connection error occurred and terminated.')
-                queue.put('quit')
-                break
+                max_retries -= 1
+                print(f"Retrying in 5 seconds...")
+                if max_retries == 0:
+                    print('A connection error occurred and terminated.')
+                    queue.put('quit')
+                    break                
+                time.sleep(5)
+            else:
+                try:
+                    root = elementTree.fromstring(stat_response.text)
+                except elementTree.ParseError as e:
+                    max_retries -= 1
+                    print(f"Retrying in 5 seconds...")
+                    if max_retries == 0:
+                        print('Terminated because xml could not be parsed.')
+                        queue.put('quit')
+                        break                    
+                    time.sleep(5)
+                else:
+                    max_retries = 3
+                    source_layer = root.find('sourceLayer').text
+                    source_stat = root.find('sourceStat').text
+                    codec_type_mapping = {'0': 'H.264/AVC', '8': 'HEVC'}
 
-            root = elementTree.fromstring(stat_response.text)
-            source_layer = root.find('sourceLayer').text
-            source_stat = root.find('sourceStat').text
-            codec_type_mapping = {'0': 'H.264/AVC', '8': 'HEVC'}
-
-            for stats in root.findall('stream'):
-                codec_type_mapping = {'0': 'H.264/AVC', '8': 'HEVC'}
-                video_codec_type = codec_type_mapping.get(
-                    stats.find('videoCodecType').text, 'Unknown')
-                video_width = stats.find('videoWidth').text
-                video_height = stats.find('videoHeight').text
-                video_rate = float(stats.find('videoRate').text)/1000000
-                frame_count = stats.find('muxedFrameCount').text
-                frame_rate = stats.find('frameRate').text
-                if not video_rate == 0.0:
-                    mux_rate = float(stats.find('muxRate').text)/1000000
-                    output_info = f"{video_codec_type} {video_width}x{video_height} {video_rate:.3f} Mbps {frame_rate} fps | Mux Rate: {mux_rate:.3f} Mbps | Frames : {frame_count}"
-            # print((chidx, source_layer, source_stat, output_info))
-            queue.put((chidx, source_layer, source_stat, output_info))
-            time.sleep(2)
+                    for stats in root.findall('stream'):
+                        codec_type_mapping = {'0': 'H.264/AVC', '8': 'HEVC'}
+                        video_codec_type = codec_type_mapping.get(
+                            stats.find('videoCodecType').text, 'Unknown')
+                        video_width = stats.find('videoWidth').text
+                        video_height = stats.find('videoHeight').text
+                        video_rate = float(stats.find('videoRate').text)/1000000
+                        frame_count = stats.find('muxedFrameCount').text
+                        frame_rate = stats.find('frameRate').text
+                        if not video_rate == 0.0:
+                            mux_rate = float(stats.find('muxRate').text)/1000000
+                            output_info = f"{video_codec_type} {video_width}x{video_height} {video_rate:.3f} Mbps {frame_rate} fps | Mux Rate: {mux_rate:.3f} Mbps | Frames : {frame_count}"
+                    
+                    if output_info is not None:
+                        queue.put((chidx, source_layer, source_stat, output_info))
+                    time.sleep(2)
 
 
 if __name__ == '__main__':
